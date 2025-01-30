39dcb218c4a816a817b77ea151db8615e49e7435
Collinsgra <kipkemoicollins941@gmail.com>
2025-01-30 11:50:01 -0800

    File publish/unpublish

--- worker.js
+++ worker.js
@@ -1,81 +1,46 @@
-/* eslint-disable import/no-named-as-default */
-import { writeFile } from 'fs';
-import { promisify } from 'util';
-import Queue from 'bull/lib/queue';
-import imgThumbnail from 'image-thumbnail';
-import mongoDBCore from 'mongodb/lib/core';
-import dbClient from './utils/db';
-import Mailer from './utils/mailer';
+import Bull from "bull"
+import imageThumbnail from "image-thumbnail"
+import { promises as fs } from "fs"
+import { ObjectID } from "mongodb"
+import dbClient from "./utils/db"
 
-const writeFileAsync = promisify(writeFile);
-const fileQueue = new Queue('thumbnail generation');
-const userQueue = new Queue('email sending');
+const fileQueue = new Bull("fileQueue")
+const userQueue = new Bull("userQueue")
 
-/**
- * Generates the thumbnail of an image with a given width size.
- * @param {String} filePath The location of the original file.
- * @param {number} size The width of the thumbnail.
- * @returns {Promise<void>}
- */
-const generateThumbnail = async (filePath, size) => {
-  const buffer = await imgThumbnail(filePath, { width: size });
-  console.log(`Generating file: ${filePath}, size: ${size}`);
-  return writeFileAsync(`${filePath}_${size}`, buffer);
-};
-
-fileQueue.process(async (job, done) => {
-  const fileId = job.data.fileId || null;
-  const userId = job.data.userId || null;
-
-  if (!fileId) {
-    throw new Error('Missing fileId');
-  }
-  if (!userId) {
-    throw new Error('Missing userId');
-  }
-  console.log('Processing', job.data.name || '');
-  const file = await (await dbClient.filesCollection())
-    .findOne({
-      _id: new mongoDBCore.BSON.ObjectId(fileId),
-      userId: new mongoDBCore.BSON.ObjectId(userId),
-    });
-  if (!file) {
-    throw new Error('File not found');
-  }
-  const sizes = [500, 250, 100];
-  Promise.all(sizes.map((size) => generateThumbnail(file.localPath, size)))
-    .then(() => {
-      done();
-    });
-});
-
-userQueue.process(async (job, done) => {
-  const userId = job.data.userId || null;
-
-  if (!userId) {
-    throw new Error('Missing userId');
-  }
-  const user = await (await dbClient.usersCollection())
-    .findOne({ _id: new mongoDBCore.BSON.ObjectId(userId) });
-  if (!user) {
-    throw new Error('User not found');
-  }
-  console.log(`Welcome ${user.email}!`);
+async function generateThumbnail(path, options) {
   try {
-    const mailSubject = 'Welcome to ALX-Files_Manager by B3zaleel';
-    const mailContent = [
-      '<div>',
-      '<h3>Hello {{user.name}},</h3>',
-      'Welcome to <a href="https://github.com/B3zaleel/alx-files_manager">',
-      'ALX-Files_Manager</a>, ',
-      'a simple file management API built with Node.js by ',
-      '<a href="https://github.com/B3zaleel">Bezaleel Olakunori</a>. ',
-      'We hope it meets your needs.',
-      '</div>',
-    ].join('');
-    Mailer.sendMail(Mailer.buildMessage(user.email, mailSubject, mailContent));
-    done();
-  } catch (err) {
-    done(err);
+    const thumbnail = await imageThumbnail(path, options)
+    const thumbnailPath = `${path}_${options.width}`
+    await fs.writeFile(thumbnailPath, thumbnail)
+  } catch (error) {
+    console.error("Error generating thumbnail:", error)
   }
-});
+}
+
+fileQueue.process(async (job) => {
+  const { userId, fileId } = job.data
+  if (!fileId) throw new Error("Missing fileId")
+  if (!userId) throw new Error("Missing userId")
+
+  const file = await dbClient.db.collection("files").findOne({
+    _id: ObjectID(fileId),
+    userId: ObjectID(userId),
+  })
+
+  if (!file) throw new Error("File not found")
+
+  const originalPath = file.localPath
+  await generateThumbnail(originalPath, { width: 500 })
+  await generateThumbnail(originalPath, { width: 250 })
+  await generateThumbnail(originalPath, { width: 100 })
+})
+
+userQueue.process(async (job) => {
+  const { userId } = job.data
+  if (!userId) throw new Error("Missing userId")
+
+  const user = await dbClient.db.collection("users").findOne({ _id: ObjectID(userId) })
+  if (!user) throw new Error("User not found")
+
+  console.log(`Welcome ${user.email}!`)
+})
